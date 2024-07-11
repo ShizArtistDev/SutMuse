07d1c5cedb9c4ba00fc050b415fa990095c026c8
Eugine <jonlavrin@gmail.com>
2024-07-11 14:29:07 +0300

    Обновление AlphaTest-0.1.3

--- config.py
+++
@@ -1,6 +0,0 @@
-settings = {
-'token': 'MTI1OTc3MTkzMDMyNDYzMTY1NA.GqQjc2.PoXzX602hQFCZpHyXNV9i7kGWKVyZ7eabR4lvI',
-'bot': 'SutMuse',
-'id': 1259771930324631654,
-'prefix': '!'
-}
---
+++ config_example.py
@@ -0,0 +1,8 @@
+settings = {
+'token': 'token',
+'bot': 'SutMuse',
+'id': token,
+'prefix': '!',
+'testsound': 'path',
+'wd': 'path'
+}
